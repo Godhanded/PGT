670f1a3a0be10873740155e2e1d687de3705e671
Godand <aezeigwe2003@gmail.com>
2023-06-24 22:01:35 +0000

    Set up Token|clean helper config

---
+++ hardhat/contracts/PGT.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
+
+contract PriceGapToken is ERC20, Ownable, ERC20Permit {
+    constructor() ERC20("Price Gap Token", "PGT") ERC20Permit("Price Gap Token") {
+        _mint(msg.sender, 1000000 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
